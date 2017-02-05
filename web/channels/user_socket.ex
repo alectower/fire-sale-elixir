7440ae001eccfc0db5c1fc2d7040b2b61a3d08ea
Alec Tower <alectower@gmail.com>
2017-02-05 15:29:54 -0500

    Update config for heroku deployments

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule FireSale.UserSocket do
   # channel "rooms:*", FireSale.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
