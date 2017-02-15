19ecc60f290cb52d6e9fcae5b4461ce43c062b99
Alec Tower <alectower@gmail.com>
2017-02-15 07:50:17 -0500

    Update mailer sender address to include display name

--- lib/fire_sale/alert_mailer.ex
+++ lib/fire_sale/alert_mailer.ex
@@ -6,7 +6,7 @@ defmodule FireSale.AlertMailer do
   def send_alert(email_address, alerts) do
     new_email
     |> put_html_layout({FireSale.LayoutView, "email.html"})
-    |> from("FireSale <alerts@firesale.com>")
+    |> from({"FireSale", "alerts@firesale.com"})
     |> to(email_address)
     |> subject("FireSale Alerts!")
     |> assign(:alerts, alerts)
